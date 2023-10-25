a0d1ef158183c193baf39b931ec67deba9911e7e
joaoguzela <joao.guzela@gmail.com>
2023-10-25 09:11:23 -0300

    chore: cliente db

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,13 @@ import fastify from 'fastify'
 import { env } from '../env/index'
 import { transactionRoutes } from './routes/transactions'
 import cookie from '@fastify/cookie'
+import { getEnvironmentData } from 'node:worker_threads'
 
 const app = fastify()
 app.register(cookie)
 
 app.register(transactionRoutes, { prefix: 'transactions' })
 
-app
-  .listen({
-    port: 10000,
-  })
-  .then(() => {
-    console.log('HTTP Server Running')
-  })
+app.listen({ host: '0.0.0.0', port: env.PORT }).then(() => {
+  console.log('HTTP Server Running')
+})
